55cf010d838745151a75daadd203288715a34b86
Kyle Ondy <kyle@ondy.org>
2020-09-16 15:04:41 -0400

    wip: working on gluster init

--- node_builder/modules/glusterfs/gluster-init.sh
+++ node_builder/modules/glusterfs/gluster-init.sh
@@ -2,14 +2,32 @@
 set -eu
 set -x
 
+###########################################################################
+#
+# GLUSTER INIT. The scrip that keeps getting scarier
+#
+# Here are the possible states the gluster service can be in when this script
+# runs.
+# 1) Volume is shared and health, shouldn't need to do anything
+# 2) No valid share (all machines rebooted at the same time), peer and create
+# 3) One or two nodes are disconnected (they were rebooted)
+#
+###########################################################################
+
 VOLUME_NAME="shared"
 SERVER_MOUNT="/gluster"
 CLIENT_MOUNT="/data"
 
+# is everything good? Lets just exit.
+
+# are things in a broken state? Try and fix it
+gluster peer status | grep 'State: Peer Rejected' -B2 | grep Hostname | cut -d" " -f2 | xargs -I{} bash -c 'yes | gluster peer detach {} force'
+gluster peer status | grep 'State: Peer Rejected' -B2 | grep Uuid | cut -d" " -f2 | xargs -I{} rm /var/lib/glusterd/peers/{}
+
 # todo: script for startup
-gluster peer probe w1
-gluster peer probe w2
-gluster peer probe w3
+gluster peer probe w1.dmz.509ely.com
+gluster peer probe w2.dmz.509ely.com
+gluster peer probe w3.dmz.509ely.com
 
 # happy path, volume exisits, just join it.
 if ! gluster volume list | grep -xq "$VOLUME_NAME"; then
