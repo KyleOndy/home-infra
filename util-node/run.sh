7380b6b2b7fec2b8127020ac736324ab011721a2
Kyle Ondy <kyle@ondy.org>
2020-12-22 21:33:40 -0500

    feat: start using nixos

--- util-node/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-# build before trying to run the images
-docker-compose build --pull
-docker-compose up -d --remove-orphans
-docker-compose logs -f --timestamps --tail 40
