8e9ffe48d27504ef7b083ae66537d57ecdb8789d
Kyle Ondy <kyle@ondy.org>
2020-12-27 01:00:39 -0500

    use newer nix bin

--- configuration.worker.nix
+++ configuration.worker.nix
@@ -42,26 +42,26 @@
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZq6q45h3OVj7Gs4afJKL7mSz/bG+KMG0wIOEH+wXmzDdJ0OX6DLeN7pua5RAB+YFbs7ljbc8AFu3lAzitQ2FNToJC1hnbLKU0PyoYNQpTukXqP1ptUQf5EsbTFmltBwwcR1Bb/nBjAIAgi+Z54hNFZiaTNFmSTmErZe35bikqS314Ej60xw2/5YSsTdqLOTKcPbOxj2kulznM0K/z/EDcTzGqc0Mcnf51NtzxlmB9NR4ppYLoi7x+rVWq04MbdAmZK70p5ndRobqYSWSKq+WDUAt2+CiTm6ItDowTLuo3zjHyYV1eCnB35DdakKVldIHrQyhmhbf5hJi6Ywx6XCzlFoNpkl/++RrJT2rf0XpGdlRoLQoKFvNRfnO4LI499SIfFb9Pwq7LhF1C1kTmshN/9S44d6VCCYXLE4uS8OPv7IXxUvFQZaIKCbomd2FzXxUwf4lg2gSlczysgDaVsMAUvlfDVgTFX8Xt1LFl3DqNtUiUpa9+Jnst/jCqqOBf3e8= kyle@alpha"
   ];
 
-  nix =
-    {
-      gc.automatic = true;
-      optimise.automatic = true;
-      buildMachines = [
-        {
-          hostName = "builder";
-          system = "x86_64-linux";
-          maxJobs = 2;
-          speedFactor = 1;
-          supportedFeatures = [ ];
-          mandatoryFeatures = [ ];
-        }
-      ];
-      distributedBuilds = true;
-      extraOptions = ''
-        builders-use-substitutes = true
-        experimental-features = nix-command
-      '';
-    };
+  nix = {
+    package = pkgs.nixUnstable;
+    gc.automatic = true;
+    optimise.automatic = true;
+    buildMachines = [
+      {
+        hostName = "builder";
+        system = "x86_64-linux";
+        maxJobs = 2;
+        speedFactor = 1;
+        supportedFeatures = [ ];
+        mandatoryFeatures = [ ];
+      }
+    ];
+    distributedBuilds = true;
+    extraOptions = ''
+      builders-use-substitutes = true
+      experimental-features = nix-command
+    '';
+  };
 
 
   # This value determines the NixOS release from which the default
