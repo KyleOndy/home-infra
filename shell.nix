f17ab179a6d7b5a235fd7cd9fc9da7ee3b1521e1
Kyle Ondy <kyle@ondy.org>
2020-12-26 23:32:47 -0500

    cleaning

--- shell.nix
+++ shell.nix
@@ -2,24 +2,20 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
-  #nixops = import "${sources.nixops.outPath}/shell.nix";
 in
 pkgs.mkShell {
 
-  buildInputs = [
-    # sources.nixops
-    pkgs.nixos-generators
+  buildInputs = with pkgs; [
+    nix-prefetch-git
 
     # pre-commit
     # https://pre-commit.com/
-    pkgs.pre-commit
-    pkgs.detect-secrets
-    pkgs.shfmt
-    pkgs.ruby
+    pre-commit
+    detect-secrets
+    shfmt
+    ruby
   ];
 
   shellHook = ''
-    shell_dir="${toString ./.}"
-    export NIX_PATH="nixpkgs=$shell_dir/vendor/nixpkgs"
   '';
 }
