5bc926e7a6b1376c0fd4ec559c39d79c92aac86f
Kyle Ondy <kyle@ondy.org>
2021-03-24 22:35:00 -0400

    start: figuring out why trafeik isn't working

--- shell.nix
+++ shell.nix
@@ -16,7 +16,7 @@ mkShell {
 
   shellHook = ''
     export NOMAD_ADDR=http://10.25.89.20:4646
-    export CONSUL_ADDR=http://10.25.89.20:8500
+    export CONSUL_HTTP_ADDR=http://10.25.89.20:8500
     export AWS_ACCESS_KEY_ID="AKIAWLHJP4A2VKY63L4X"
     export AWS_SECRET_ACCESS_KEY="$(pass show aws.amazon.com/ondy-org/svc.home-infra/AKIAWLHJP4A2VKY63L4X)"
   '';
